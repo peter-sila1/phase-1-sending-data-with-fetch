ec1f785d92befac20b23b9be634236246649569e
peter-sila1 <peter.sila@student.moringaschool.com>
2023-08-07 15:04:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = { name, email };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, config)
+      .then(response => response.json())
+      .then(data => {
+        appendIdToDOM(data.id);
+        return data;
+      })
+      .catch(error => {
+        displayErrorToDOM(error.message);
+      });
+  }
+  
+  function appendIdToDOM(id) {
+    const responseElement = document.createElement('div');
+    responseElement.textContent = `Response ID: ${id}`;
+    responseElement.id = 'responseId';
+    document.body.appendChild(responseElement);
+  }
+  
+  function displayErrorToDOM(errorMessage) {
+    const errorElement = document.createElement('div');
+    errorElement.textContent = `Error: ${errorMessage}`;
+    errorElement.id = 'errorId';
+    document.body.appendChild(errorElement);
+  }
+  
